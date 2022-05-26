6a209e50b7d9e1a75737f9211d4db43f0ed8e68c
minhaz2597 <md.minhazuddin2597@gmail.com>
2022-05-27 01:06:17 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mav3s.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("computer_parts");
+    const productCollection = database.collection("products");
+    console.log("Database connected");
+
+    // GET products API
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find({});
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Par-T-ake!");
+});
+
+app.listen(port, () => {
+  console.log(`par-T-ake app listening on port ${port}`);
+});
